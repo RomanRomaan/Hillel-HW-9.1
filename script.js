2785b156f441f8c0c333d256869bb62269924a31
RomanRomaan <jay4or@gmail.com>
2025-07-03 13:21:50 +0300

    final

---
+++ script.js
@@ -0,0 +1,30 @@
+/**
+ * Дізнатись суму всіх зарплат користувачів:
+ * Об'єкт може містити невідому кількість департаментів та співробітників
+ */
+let company = {
+    sales: [
+        { name: 'John', salary: 1000 },
+        { name: 'Alice', salary: 600 }
+    ],
+    development: {
+        web: [
+            { name: 'Peter', salary: 2000 },
+            { name: 'Alex', salary: 1800 }
+        ],
+        internals: [
+            { name: 'Jack', salary: 1300 }
+        ]
+    }
+};
+
+function sumSalaries(department) {
+    if (Array.isArray(department)) {
+        return department.reduce((acc, curr) => acc + curr.salary, 0);
+    } else {
+        return Object.values(department).reduce((acc, curr) => acc + sumSalaries(curr), 0);
+    }
+
+}
+
+console.log(sumSalaries(company))
